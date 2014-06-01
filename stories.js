e86a19a63b395a760caff5dac1a0134982fd9403
Theresa Luu <theresa.luu@gmail.com>
2014-06-01 16:55:10 -0700

    did a blog preview. need to write real one

--- stories.js
+++ stories.js
@@ -4,7 +4,7 @@ on_stories(
         "head_html": "<a href='cities/Orange County'>In Orange County</a>",
         "content_html": "Civic- minded residents gather to organize and use tech to make positive changes in the community.",
         "image source": "http://photos2.meetupstatic.com/photos/event/b/e/c/a/highres_340608842.jpeg",
-        "image_src": "media/images/homepage-stories/highres_340608842.jpg"
+        "image_src": "media/images/homepage-stories/hack4change.jpg"
     }
 ]
 );
